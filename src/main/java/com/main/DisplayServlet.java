2ce119d9dd5a170ee75afaf9c9f0573149821a39
Rajkumar <rajkumar67272@gmail.com>
2021-08-16 13:50:30 +0530

    change the request to get

--- src/main/java/com/main/DisplayServlet.java
+++ src/main/java/com/main/DisplayServlet.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 public class DisplayServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("Hi");
 	}
 
